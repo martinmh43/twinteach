d781939e3ff1d6c227a9fb26a031c4ed8d96db09
Martín <martinmh43@educastur.es>
2024-03-11 16:28:18 +0100

    ej1

---
+++ querrys.sql
@@ -0,0 +1,13 @@
+
+/*
+1. Crear cuenta bbdd (alumnoEjercicioBBDD, ejercicioBBDD@gmail.com, 123456789,
+ejercicioBBDD, ApellidosEjercicioBBDD) -> Insert
+*/
+
+INSERT INTO Alumnos (nombre_apellidos, nick, contraseña, correo, url_imagen)
+VALUES ("alumnoEjercicioBBDD", "ejercicioBBDD", "123456789", "ejercicioBBDD@gmail.com", "url_example");
+
+/*
+
+*/
+
