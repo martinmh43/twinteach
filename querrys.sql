b12ff4cc785742f16405ecfa8580ca986f72f2b2
Martín <martinmh43@educastur.es>
2024-03-11 16:35:27 +0100

    ej5_6

--- querrys.sql
+++ querrys.sql
@@ -5,7 +5,7 @@ ejercicioBBDD, ApellidosEjercicioBBDD) -> Insert
 */
 
 INSERT INTO Alumnos (nombre_apellidos, nick, contraseña, correo, url_imagen)
-VALUES ("alumnoEjercicioBBDD", "ejercicioBBDD", "123456789", "ejercicioBBDD@gmail.com", "url_example");
+VALUES ("alumnoEjercicioBBDD", "ejercicioBBDD", "123456789", "ejercicioBBDD@gmail.com", "url_example1");
 
 /*
 2. Log in como alumno
@@ -16,6 +16,18 @@ FROM Alumnos
 WHERE nombre_apellidos="alumnoEjercicioBBDD" and contraseña="123456789";
 
 
-/*
 
+/*
+4. Crear cuenta como profesor (profesorEjercicioBBDD...)
 */
+INSERT INTO Profesores (nombre_apellidos,nick,contraseña,correo,url_imagen)
+VALUES ("profesorEjercicioBBDD", "profEjercicioBBDD", "12345", "profesorEjercicioBBDD@gmail.com", "url_example2");
+
+
+/*
+5. Log in como profesor
+*/
+
+SELECT nombre_apellidos, contraseña 
+FROM Profesores
+WHERE nombre_apellidos="profesorEjercicioBBDD" and contraseña="12345";
